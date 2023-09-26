67747df1dc04e243d1866eb792f4f801135b63e0
Rob Wong <rob@dylibso.com>
2023-09-26 16:43:29 -0600

    fix: include allocation functions

--- no-extism-guest/src/lib.rs
+++ no-extism-guest/src/lib.rs
@@ -1,18 +1,38 @@
+use std::mem;
 use std::ptr;
 use std::slice;
 
 #[no_mangle]
 pub extern "C" fn say_hello(mem_addr: *const u8, len: usize, ret_len_addr: *mut u32) -> i32 {
+    // read input string
     let data: Vec<u8> = unsafe { slice::from_raw_parts(mem_addr, len).to_vec() };
+
+    // create output string
     let greeting = format!(
         "Hello there, {}.  That was a lot of memory management to pass a string!",
         String::from_utf8_lossy(&data)
     );
 
-    // determine the length of the modified string and set that value to the in/out ret_len_addr
+    // determine the length of the output string and set that value in the address of ret_len_addr
     unsafe {
         ptr::write(ret_len_addr, greeting.len() as u32);
     };
 
     greeting.as_ptr() as i32
+}
+
+#[no_mangle]
+pub extern "C" fn alloc() -> *const u8 {
+    let mut buf = Vec::with_capacity(1024);
+    let ptr = buf.as_mut_ptr();
+
+    // tell Rust not to clean this up
+    mem::forget(buf);
+
+    ptr
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn dealloc(ptr: &mut u8) {
+    let _ = Vec::from_raw_parts(ptr, 0, 1024);
 }
--- no-extism-host/src/main.rs
+++ no-extism-host/src/main.rs
@@ -13,34 +13,40 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     )?;
     let instance = Instance::new(&mut store, &module, &[])?;
 
-    // get an instance of Linear Memory
+    // get the guest's exported functions
+    let say_hello = instance.get_typed_func::<(i32, i32, i32), i32>(&mut store, "say_hello")?;
+    let alloc = instance.get_typed_func::<(), i32>(&mut store, "alloc")?;
+    let dealloc = instance.get_typed_func::<i32, ()>(&mut store, "dealloc")?;
+
+    // get an instance of linear memory
     let memory = instance
         .get_memory(&mut store, "memory")
         .ok_or(anyhow::format_err!("failed to find `memory` export"))?;
 
+    // allocate memory to hold the input string and the output string return length
+    let input_addr = alloc.call(&mut store, ())?;
+    let output_len_addr = alloc.call(&mut store, ())?;
+
+    // define the input string and write the string as binary to the memory we allocated for the input string
     let input_string = b"non-extism host!";
-    // write the string as binary to location 0 in Linear Memory
-    memory.write(&mut store, 0, input_string)?;
+    memory.write(&mut store, input_addr as usize, input_string)?;
 
-    // get our TypedFunction, `say_hello` from the WebAssembly exports
-    let say_hello = instance.get_typed_func::<(i32, i32, i32), i32>(&mut store, "say_hello")?;
-
-    // invoke the `say_hello` function
-    let ptr = say_hello.call(
+    // invoke the `say_hello` function and save the memory address to where the output will be saved
+    let output_addr = say_hello.call(
         &mut store,
         (
-            0,                               // input memory address
-            input_string.len() as i32,       // input length
-            (input_string.len() + 1) as i32, // output length memory address
+            input_addr,                // input memory address
+            input_string.len() as i32, // input length
+            output_len_addr as i32,    // output length memory address
         ),
     )?;
 
-    // we are storing the length of the output string as an unsigned 32 bit integer in Linear Memory, so let's
-    // allocate 4 bytes, read the data from Linear Memory, and store it in the len_buffer
+    // we are storing the length of the output string as an unsigned 32 bit integer in linear memory, so let's
+    // allocate 4 bytes, read the data from linear memory, and store it in the len_buffer
     let mut len_buffer = [0u8; 4];
-    memory.read(&store, input_string.len() + 1, &mut len_buffer)?;
+    memory.read(&store, output_len_addr as usize, &mut len_buffer)?;
 
-    // now convert the bytes into an i32 (WebAssembly is always little endian)
+    // convert the bytes into an i32 (WebAssembly is always little endian)
     let len = i32::from_le_bytes(len_buffer);
 
     // let's now create a new Vec<u8> to store the bytes of the output string and fit it to size
@@ -48,11 +54,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     v.resize(len as usize, 0);
 
     // read the memory address returned to us by the `say_hello` function, which points to the location
-    // in Linear Memory where our WebAssembly module stored the output string
-    memory.read(&store, ptr.try_into().unwrap(), &mut v)?;
+    // in linear memory where our WebAssembly module stored the output string
+    memory.read(&store, output_addr.try_into().unwrap(), &mut v)?;
 
     // decode the byte vector into a UTF8 string and print to the console!
     println!("{}", String::from_utf8_lossy(&v));
+
+    // deallocate all of the memory that we allocated
+    // this is technically unnecessary because the program is about to end, but that may not always be the case
+    dealloc.call(&mut store, input_addr)?;
+    dealloc.call(&mut store, output_len_addr)?;
 
     Ok(())
 }
